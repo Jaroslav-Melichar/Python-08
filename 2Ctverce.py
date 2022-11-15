db9f5f02bd81b525051c3f898e2d130a26cbd8bb
Jaroslav-Melichar <79139968+Jaroslav-Melichar@users.noreply.github.com>
2022-11-15 15:15:03 +0100

    Update 2Ctverce.py

---
+++ 2Ctverce.py
@@ -0,0 +1,10 @@
+import turtle 
+t = turtle.Turtle()
+t.shape('turtle')
+print("čtverec ve čtverci")
+for cycle in range(4):
+    t.forward(100)
+    t.left(90)
+    for cycle in range(4):
+        t.forward(50)
+        t.left(90)
